5988f8c6209e254abc0fe28c754228a0b6ed7c11
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2026-09-14 11:34:37 -0300

    refactor: change database to function-based factory pattern

--- app/database.py
+++ app/database.py
@@ -1,21 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect("users.db")
+DB_NAME = "users.db"
 
-cursor = conn.cursor()
 
-try:
-    cursor.execute("""
-        CREATE TABLE users (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL,
-            email TEXT NOT NULL,
-            password TEXT NOT NULL,
-            created_at DATE NOT NULL
-        );
-    """)
+def get_connection():
+    return sqlite3.connect(DB_NAME)
 
-    conn.close()
-    print("O banco foi criado com sucesso.")
-except sqlite3.OperationalError:
-    print("O banco já foi criado.")
+
+def init_db():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("""
+                CREATE TABLE users (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    username TEXT NOT NULL,
+                    email TEXT NOT NULL,
+                    password TEXT NOT NULL,
+                    created_at DATE NOT NULL
+                );
+            """)
+            print("O banco foi criado/verificado com sucesso.")
+        except sqlite3.OperationalError as e:
+            print("Erro ao inicializar o banco", e)
